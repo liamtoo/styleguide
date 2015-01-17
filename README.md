9fb1e1678c3b957dcfb738583cebc30650126e03
Vladimir Skok <skoksworks@gmail.com>
2015-01-16 16:32:00 -0800

    clean and restructure project

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# styleguide
+# RSG Styleguide
 
-Styleguide tests 
+## Hologram
 
-## Installation 
 
-`sudo npm install` - npm dependencies 
-`gem install hologram` - hologram gem 
-
-## Sassdoc tests 
-
-`gulp-styleguide/assets/tools` - source
-
-`gulp-styleguide/sassdoc` - generated documentation 
-
-## Hologram tests 
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var 	gulp 			  = require('gulp'),
+	sass 			    = require('gulp-ruby-sass'),
+	autoprefixer 	= require('gulp-autoprefixer'),
+	sassdoc			  = require('gulp-sassdoc'),
+  	hologram      = require('gulp-hologram');
+
+gulp.task('default', function() {
+	return gulp.src('sass/main.scss')
+    		.pipe(sass({
+    			style: 'expanded',
+    			precision: 1,
+        		lineNumbers : true
+    	}))
+    	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    	.pipe(gulp.dest('build/'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('**/*.scss', ['default']);
+});
+
+/**
+*
+* Default sassDoc task
+* Config options - http://sassdoc.com/gulp/
+* API - http://sassdoc.com/annotations/
+*
+**/
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src('sass/tools')
+    .pipe(sassdoc({
+        'dest': 'build/sassdoc',
+        'verbose': true,
+        'groups': {
+          undefined: "undefined",
+        },
+    'display': {
+      'access': ['public', 'private'],
+      'alias': true
+    },
+        'basePath': 'https://github.com/Skoks/styleguide'
+    }));
+});
+
+/**
+*
+* Default hologram task
+* https://github.com/rejahrehim/gulp-hologram
+*
+**/
+
+gulp.task('hologram', function() {
+        gulp.src('./hologram/config.yml')
+          .pipe(hologram());
+});
